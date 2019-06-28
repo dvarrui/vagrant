3385cc8d2369740b9b2e78d2e8fb07854dcb738d
David <dvarrui@protonmail.com>
2019-06-28 10:44:56 +0100

    U

--- check-installations/debian.rb
+++ check-installations/debian.rb
@@ -20,3 +20,29 @@ group "Debian (t-node)" do
   expect [ "teuton", "2.0" ]
 
 end
+
+group "Debian (panel)" do
+
+  dir = "panel/debian"
+
+  goto :localhost, :exec => "sh -c \"cd #{dir} && vagrant up\""
+
+  target "vagrant status"
+  goto :localhost, :exec => "sh -c \"cd #{dir} && vagrant status\""
+  expect [ "teuton-panel-debian", "running" ]
+
+  target "uname -a"
+  goto :localhost, :exec => "cd #{dir} && vagrant ssh -c \"uname -a\""
+  expect [ "Debian", "panel" ]
+
+  target "teuton-panel"
+  goto :localhost, :exec => "cd #{dir} && vagrant ssh -c \"vdir /usr/local/bin\""
+  expect "teuton-panel"
+
+  target "/opt/teuton-panel"
+  goto :localhost, :exec => "cd #{dir} && vagrant ssh -c \"vdir /opt\""
+  expect "teuton-panel"
+
+  goto :localhost, :exec => "sh -c \"cd #{dir} && vagrant halt\""
+  goto :localhost, :exec => "sh -c \"cd #{dir} && vagrant destroy\""
+end
